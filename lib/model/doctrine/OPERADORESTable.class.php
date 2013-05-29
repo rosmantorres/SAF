ed0e0656268e106e185a6effb343c586e5f126d4
rt999 <rt999@ATI-003905.GRUPOCCO.COM>
2013-05-29 09:50:57 -0400

    - Agregandole el método getProgramadores()

--- lib/model/doctrine/OPERADORESTable.class.php
+++ lib/model/doctrine/OPERADORESTable.class.php
@@ -16,4 +16,25 @@ class OPERADORESTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('OPERADORES');
     }
+    
+    /**
+     * Método que retorna un array de operadores (programador y operador_responsable)
+     * 
+     * @param integer $num_proposicion
+     * @return array
+     */
+    public function getOperadores($num_proposicion)
+    {
+      $hist_proposicion = Doctrine_Core::getTable('HIST_PROPOSICIONES')
+              ->getHistProposicion($num_proposicion);
+      
+      $operadores = array();
+      
+      if ($hist_proposicion){
+        array_push($operadores, $this->find($hist_proposicion->getOperCodOperadorAsig()));
+        array_push($operadores, $this->find($hist_proposicion->getOperCodOperadorResp()));
+      }
+      
+      return $operadores;
+    }
 }

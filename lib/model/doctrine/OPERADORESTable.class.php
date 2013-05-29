15086bbe1af4d8f4c1bf30cfb2e89a8d9a6131e4
rt999 <rt999@ATI-003905.GRUPOCCO.COM>
2013-05-29 13:25:50 -0400

    - Aplicando estándares symfony

--- lib/model/doctrine/OPERADORESTable.class.php
+++ lib/model/doctrine/OPERADORESTable.class.php
@@ -7,34 +7,37 @@
  */
 class OPERADORESTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object OPERADORESTable
-     */
-    public static function getInstance()
+
+  /**
+   * Returns an instance of this class.
+   *
+   * @return object OPERADORESTable
+   */
+  public static function getInstance()
+  {
+    return Doctrine_Core::getTable('OPERADORES');
+  }
+
+  /**
+   * Método que retorna un array de operadores (programador y operador_responsable)
+   * 
+   * @param integer $num_proposicion
+   * @return array
+   */
+  public function getOperadores($num_proposicion)
+  {
+    $hist_proposicion = Doctrine_Core::getTable('HIST_PROPOSICIONES')
+            ->getHistProposicion($num_proposicion);
+
+    $operadores = array();
+
+    if ($hist_proposicion)
     {
-        return Doctrine_Core::getTable('OPERADORES');
+      array_push($operadores, $this->find($hist_proposicion->getOperCodOperadorAsig()));
+      array_push($operadores, $this->find($hist_proposicion->getOperCodOperadorResp()));
     }
-    
-    /**
-     * Método que retorna un array de operadores (programador y operador_responsable)
-     * 
-     * @param integer $num_proposicion
-     * @return array
-     */
-    public function getOperadores($num_proposicion)
-    {
-      $hist_proposicion = Doctrine_Core::getTable('HIST_PROPOSICIONES')
-              ->getHistProposicion($num_proposicion);
-      
-      $operadores = array();
-      
-      if ($hist_proposicion){
-        array_push($operadores, $this->find($hist_proposicion->getOperCodOperadorAsig()));
-        array_push($operadores, $this->find($hist_proposicion->getOperCodOperadorResp()));
-      }
-      
-      return $operadores;
-    }
+
+    return $operadores;
+  }
+
 }

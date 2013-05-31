ddd8d2294a756ecfcaad7835f6ce215be423c9cb
rt999 <rt999@ATI-003905.GRUPOCCO.COM>
2013-05-31 15:09:05 -0400

    - Modificando el partial con datos en primer plano y datos en segundo plano

--- apps/frontend/modules/agenda_convocatoria/templates/_eventos.php
+++ apps/frontend/modules/agenda_convocatoria/templates/_eventos.php
@@ -8,22 +8,22 @@
     <tr>
       <th>NUM_F328</th>
       <th>Fecha Inicio</th>
-      <th>Fecha Reparacion</th>
+      <!--<th>Fecha Reparacion</th>-->
       <th>Region</th>
       <th>Circuito</th>
-      <th>Nivel</th>
-      <th>Kva Int</th>
+      <!--<th>Nivel</th>-->
+      <!--<th>Kva Int</th>-->
       <th>Mva Min</th>
-      <th>Num Averia</th>
-      <th>Desc Averia</th>
-      <th>Tipo Falla</th>
-      <th>Operador</th>
-      <th>Cuadrilla</th>
-      <th>Climatologia</th>
+      <!--<th>Num Averia</th>-->
+      <!--<th>Desc Averia</th>-->
+      <!--<th>Tipo Falla</th>-->
+      <!--<th>Operador</th>-->
+      <!--<th>Cuadrilla</th>-->
+      <!--<th>Climatologia</th>-->
       <th>Trabajo Realizado</th>
-      <th>Num Roe</th>
-      <th>Programador</th>
-      <th>Operador Resp</th>
+      <!--<th>Num Roe</th>-->
+      <!--<th>Programador</th>-->
+      <!--<th>Operador Resp</th>-->
     </tr>
   </thead>
   <tbody>
@@ -37,25 +37,30 @@
       <?php if ($evento->getTipoFalla() == 'CAUSA-500'): ?>
         <tr class="info">
       <?php endif; ?>
-          <td><?php echo $evento->getCEventoD() ?></td>
+          <td>
+            <a href="#<?php echo $evento->getCEventoD() ?>" data-toggle="modal">
+              <?php echo $evento->getCEventoD() ?>              
+            </a>
+            <?php include_partial('modal',array('evento' => $evento)) ?>
+          </td>
           <td><?php echo $evento->getFHoraIni() ?></td>
-          <td><?php echo $evento->getFHoraRep() ?></td>
+          <!--<td><?php // echo $evento->getFHoraRep() ?></td>-->
           <td><?php echo $evento->getRegion() ?></td>
           <td><?php echo $evento->getCircuito() ?></td>
-          <td><?php echo $evento->getCodNivel() ?></td>
-          <td><?php echo $evento->getKvaInt() ?></td>
+          <!--<td><?php // echo $evento->getCodNivel() ?></td>-->
+          <!--<td><?php // echo $evento->getKvaInt() ?></td>-->
           <td><?php echo $evento->getMvaMin() ?></td>
-          <td><?php echo $evento->getNumAveria() ?></td>
-          <td><?php echo $evento->getDescAveria() ?></td>
-          <td><?php echo $evento->getTipoFalla() ?></td>
-          <td><?php echo $evento->getOperador() ?></td>
-          <td><?php echo $evento->getCuadrilla() ?></td>
-          <td><?php echo $evento->getClimatologia() ?></td>
+          <!--<td><?php // echo $evento->getNumAveria() ?></td>-->
+          <!--<td><?php // echo $evento->getDescAveria() ?></td>-->
+          <!--<td><?php // echo $evento->getTipoFalla() ?></td>-->
+          <!--<td><?php // echo $evento->getOperador() ?></td>-->
+          <!--<td><?php // echo $evento->getCuadrilla() ?></td>-->
+          <!--<td><?php // echo $evento->getClimatologia() ?></td>-->
           <td><?php echo $evento->getTrabajoRealizado() ?></td>
-          <td><?php echo $evento->getNumRoe() ?></td>
-          <td><?php echo $evento->getProgramador() ?></td>
-          <td><?php echo $evento->getOperadorResp() ?></td>
+          <!--<td><?php // echo $evento->getNumRoe() ?></td>-->
+          <!--<td><?php // echo $evento->getProgramador() ?></td>-->
+          <!--<td><?php // echo $evento->getOperadorResp() ?></td>-->
         </tr>
     <?php endforeach ?>
   </tbody>
-</table>
+</table

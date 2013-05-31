da1f20d6d265d6ef4372774d1888bf42786f0de7
rt999 <rt999@ATI-003905.GRUPOCCO.COM>
2013-05-31 09:47:21 -0400

    - Modificando la plantilla

--- apps/frontend/modules/agenda_convocatoria/templates/filtrarSuccess.php
+++ apps/frontend/modules/agenda_convocatoria/templates/filtrarSuccess.php
@@ -1,3 +1,15 @@
-<?php if(count($eventos_imp) > 0) : ?>
-<?php include_partial('eventos',array('eventos' => $eventos_imp)); ?>
-<?php endif; ?>
+<div class="accordion" id="accordion">
+  
+  <?php if (count($eventos_imp) > 0) : ?>
+    <?php include_partial('acordeon', array('id_acordeon' => '1', 'cabecera' => 'INTERRUPCIONES IMPREVISTAS', 'contenido' => $eventos_imp)) ?>
+  <?php endif; ?>
+  
+  <?php if (count($eventos_pro) > 0) : ?>
+    <?php include_partial('acordeon', array('id_acordeon' => '2', 'cabecera' => 'INTERRUPCIONES PROGRAMADAS', 'contenido' => $eventos_pro)) ?>
+  <?php endif; ?>
+  
+  <?php if (count($eventos_500) > 0) : ?>
+    <?php include_partial('acordeon', array('id_acordeon' => '3', 'cabecera' => 'INTERRUPCIONES CAUSAS 500', 'contenido' => $eventos_500)) ?>
+  <?php endif; ?>
+  
+</div>

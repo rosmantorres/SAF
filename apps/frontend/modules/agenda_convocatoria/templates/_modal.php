73f2df9f5e468798e86a200396b2861c4d7746b3
Rosman <Rosman@192.168.0.109>
2013-06-02 13:57:34 -0430

    - Modificando la descripción del evento en segundo plano

--- apps/frontend/modules/agenda_convocatoria/templates/_modal.php
+++ apps/frontend/modules/agenda_convocatoria/templates/_modal.php
@@ -3,28 +3,37 @@
 
 <!-- Modal -->
 <div id="<?php echo $evento->getCEventoD() ?>" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="myModalLabel">
-      Interrupción <?php echo $evento->getCEventoD() ?>
-    </h3>
+    <h3 id="myModalLabel">Interrupción <?php echo $evento->getCEventoD() ?></h3>    
   </div>
-  <div class="modal-body">    
-    <b><u>Fecha y hora de reparación: </u></b><?php echo $evento->getFHoraRep() ?>
-    <hr> 
-    <b>Nivel de sistema:<b><?php echo $evento->getCodNivel() ?>
-    <h5>Kva Interrumpidos:</h5><?php echo $evento->getKvaInt() ?>    
-    <h5>Operador:</h5><?php echo $evento->getOperador() ?>
-    <h5>Cuadrilla</h5><?php echo $evento->getCuadrilla() ?>
-    <h5>Climatología</h5><?php echo $evento->getClimatologia() ?>
-    <h5>Numero de reporte operacional</h5><?php echo $evento->getNumRoe() ?>
-    <h5>Programador Parada</h5><?php echo $evento->getProgramador() ?>
-    <h5>Operador Responsable Parada</h5><?php echo $evento->getOperadorResp() ?>
+
+  <div class="modal-body">
+    <table class="table table-bordered" width="100%">
+      <tr><td colspan="2"><b>Fecha de reparación:</b> <?php echo $evento->getFHoraRep() ?></td></tr>
+      <tr>
+        <td>          
+          <b><u>Número de ROE:</u></b> <?php echo $evento->getNumRoe() ?><br>
+          <b><u>Nivel de sistema:</u></b> <?php echo $evento->getCodNivel() ?><br>
+          <b><u>Kva Interrumpidos:</u></b> <?php echo $evento->getKvaInt() ?></td>
+        <td>
+          <b><u>Climatologia:</u></b> <?php echo $evento->getClimatologia() ?><br>
+          <b><u>Operador:</u></b> <?php echo $evento->getOperador() ?><br>          
+          <b><u>Cuadrilla:</u></b> &nbsp;<?php echo $evento->getCuadrilla() ?><br>          
+        </td>
+      </tr>
+    </table>
     <hr>
-    <h5>Averia:</h5><?php echo $evento->getNumAveria() ?>
+    <b><u>Programador Parada:</u></b> &nbsp;&nbsp;&nbsp;<?php echo $evento->getProgramador() ?><br>
+    <b><u>Operador resp Parada:</u></b> <?php echo $evento->getOperadorResp() ?>
+    <hr>
+    <b><u>Averia:</u></b> <?php echo $evento->getNumAveria() ?>
     <p><?php echo $evento->getDescAveria() ?></p>            
-  </div>
+  </div>  
+
   <div class="modal-footer">
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Cerrar</button>
   </div>
+
 </div>

7126dedf61e023de861f328985a8033922d57aaa
Rosman <Rosman@192.168.0.109>
2013-06-02 15:10:02 -0430

    - Modificandolo hasta ahora como se quiere tener la vista

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,18 +1,67 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="es">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
+    <meta charset="utf-8">
+    <?php include_stylesheets() ?>    
+    <?php include_javascripts() ?>
     <title>
       <?php if (!include_slot('title')): ?>
         SAF 
       <?php endif; ?>
     </title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div class="container">
+      
+      <div class="masthead">
+        <h3 class="muted">SISTEMA DE ANALISIS DE FALLAS</h3>
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Downloads</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- /.navbar -->
+      </div>
+
+      <!-- jumbotron -->
+      <div>
+        <?php echo $sf_content ?>
+      </div>
+
+<!--      <hr>
+       Example row of columns 
+      <div class="row-fluid">
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>-->
+
+      <hr>
+      <div class="footer">
+        <p>&copy; SAF 2013</p>
+      </div>
+
+    </div> <!-- /container -->
   </body>
 </html>

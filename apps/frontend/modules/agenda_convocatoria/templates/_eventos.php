d634b4197b13b819317c0462fca133cc442e1657
Rosman <Rosman@192.168.0.109>
2013-06-02 17:45:20 -0430

    - Acomodando la interfaz para el usuario, con los datos de primer plano

--- apps/frontend/modules/agenda_convocatoria/templates/_eventos.php
+++ apps/frontend/modules/agenda_convocatoria/templates/_eventos.php
@@ -1,29 +1,17 @@
 <!-- 
  Elemento parcial que se encarga de mostrar los eventos en una tabla con los
- datos necesarios por el cliente
+ datos principales y con opcion de ver los datos secundarios.
 -->
 
-<table border ="1" class="table table-hover">
+<table border ="0" class="table table-bordered table-hover">
   <thead>
     <tr>
-      <th>NUM_F328</th>
-      <th>Fecha Inicio</th>
-      <!--<th>Fecha Reparacion</th>-->
-      <th>Region</th>
-      <th>Circuito</th>
-      <!--<th>Nivel</th>-->
-      <!--<th>Kva Int</th>-->
-      <th>Mva Min</th>
-      <!--<th>Num Averia</th>-->
-      <!--<th>Desc Averia</th>-->
-      <!--<th>Tipo Falla</th>-->
-      <!--<th>Operador</th>-->
-      <!--<th>Cuadrilla</th>-->
-      <!--<th>Climatologia</th>-->
-      <th>Trabajo Realizado</th>
-      <!--<th>Num Roe</th>-->
-      <!--<th>Programador</th>-->
-      <!--<th>Operador Resp</th>-->
+      <th># F328</th>
+      <th>FECHA</th>      
+      <th>REGIÓN</th>
+      <th>CIRCUITO</th>
+      <th>MVA</th>
+      <th>TRABAJO REALIZADO</th>     
     </tr>
   </thead>
   <tbody>
@@ -43,23 +31,11 @@
             </a>
             <?php include_partial('modal',array('evento' => $evento)) ?>
           </td>
-          <td><?php echo $evento->getFHoraIni() ?></td>
-          <!--<td><?php // echo $evento->getFHoraRep() ?></td>-->
+          <td><?php echo $evento->getFHoraIni() ?></td>          
           <td><?php echo $evento->getRegion() ?></td>
-          <td><?php echo $evento->getCircuito() ?></td>
-          <!--<td><?php // echo $evento->getCodNivel() ?></td>-->
-          <!--<td><?php // echo $evento->getKvaInt() ?></td>-->
-          <td><?php echo $evento->getMvaMin() ?></td>
-          <!--<td><?php // echo $evento->getNumAveria() ?></td>-->
-          <!--<td><?php // echo $evento->getDescAveria() ?></td>-->
-          <!--<td><?php // echo $evento->getTipoFalla() ?></td>-->
-          <!--<td><?php // echo $evento->getOperador() ?></td>-->
-          <!--<td><?php // echo $evento->getCuadrilla() ?></td>-->
-          <!--<td><?php // echo $evento->getClimatologia() ?></td>-->
-          <td><?php echo $evento->getTrabajoRealizado() ?></td>
-          <!--<td><?php // echo $evento->getNumRoe() ?></td>-->
-          <!--<td><?php // echo $evento->getProgramador() ?></td>-->
-          <!--<td><?php // echo $evento->getOperadorResp() ?></td>-->
+          <td><?php echo $evento->getCircuito() ?></td>          
+          <td><?php echo $evento->getMvaMin() ?></td>          
+          <td><?php echo $evento->getTrabajoRealizado() ?></td>          
         </tr>
     <?php endforeach ?>
   </tbody>

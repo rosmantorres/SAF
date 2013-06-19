6a8e76af1fba4818ad93e33cc93467b68966e5c9
Rosman Torres - Trabajo <>
2013-06-19 14:59:49 -0400

    - Modificando la clase js del módulo de minuta

--- web/js/minuta.js
+++ web/js/minuta.js
@@ -1,42 +1,184 @@
 var cant_fotos = 0;
+var cant_razones = 0;
+var cant_compromisos = 0;
+
+function VerificarCant()
+{
+  if (cant_fotos == 0)
+    jQuery('#remover_imagen').hide();
+
+  if (cant_fotos > 0)
+    jQuery('#remover_imagen').show();
+
+  if (cant_razones == 0)
+    jQuery('#remover_razon').hide();
+
+  if (cant_razones > 0)
+    jQuery('#remover_razon').show();
+  
+  if (cant_compromisos == 0)
+    jQuery('#remover_compromiso').hide();
+
+  if (cant_compromisos > 0)
+    jQuery('#remover_compromiso').show();
+}
+
+function Agregar(en, data)
+{
+  if (en == 'imagen')
+  {
+    campo = '<div id="imagen_agregada">\n\
+              <input type="text" class="span4" name="titulo_foto' + cant_fotos + '" placeholder="Indique el titulo" required /><br>\n\
+              <input type="text" class="span6" name="sub_titulo_foto' + cant_fotos + '" placeholder="Indique el sub-titulo" /><br>\n\
+              <input type="file" name="foto' + cant_fotos + '" required>\n\
+             </div>';
+    jQuery('#imagenes').append(campo);
+  }
+
+  if (en == 'razon')
+  {
+    campo = '<div id="razon_agregada">\n\
+               <input type="text" class="span3" name="razon' + cant_razones + '" data-provide="typeahead" data-items="3" data-matcher="" data-source=[' + data + '] placeholder="Indique razón" required />\n\
+               <input type="number" class="span1" name="mva_razon' + cant_razones + '" placeholder="Cant" required/> MvaMin\n\
+             </div>';
+
+    jQuery('#razones').append(campo);
+  }
+
+  if (en == 'acciones')
+  {
+    campo = '<div id="accion_agregada">\n\
+               <textarea name="acciones" class="input-block-level" rows="5" placeholder="Indique las acciones y recomendaciones" required></textarea>\n\
+             </div>';
+
+    jQuery('#acciones').append(campo);
+  }
+
+  if (en == 'compromiso')
+  {     
+    var input = ' ';
+    for (var i=1; i<= data; i++)
+    {
+      input = input + '<input type="text" class="span3" name="responsable_compromiso' + cant_compromisos + i +'" data-provide="typeahead" data-items="3" data-matcher="" data-source=[""] placeholder="Indique el responsable del compromiso" required /> ';
+    }
+      
+    campo = '<div id="compromiso_agregado">\n\
+               <textarea name="compromiso' + cant_compromisos + '" class="input-block-level" rows="3" placeholder="Indique el compromiso" required></textarea> '
+               + input +
+            '</div>';
+    
+    jQuery('#compromisos').append(campo);
+  }
+}
 
 jQuery('document').ready(function()
 {
   jQuery('#agregar_imagen').click(function()
   {
     cant_fotos++;
-    
-    campo = '<div id="imagen_agregada">\n\
-         <hr>\n\
-         <input type="text" class="span4" name="titulo_foto' + cant_fotos + '" placeholder="Indique el titulo" required /><br>\n\
-         <input type="text" class="span6" name="sub_titulo_foto'+cant_fotos+'" placeholder="Indique el sub-titulo" /><br>\n\
-         <input type="file" name="foto'+cant_fotos+'" required /></div>';
 
-    jQuery('#imagen').append(campo);
-    
-    VerificarCantFotos();
-    
+    Agregar('imagen');
+
+    VerificarCant();
+
     return false;
   });
 
   jQuery('#remover_imagen').click(function()
   {
     cant_fotos--;
-    
+
     jQuery('#imagen_agregada:last-child').remove();
+
+    VerificarCant();
+
+    return false;
+  });
+
+  jQuery('#agregar_razon').click(function()
+  {
+    $.ajax({
+      type: "POST",
+      url: "minuta/prueba",
+      data: {},
+      success: function(respuesta) {
+        if (respuesta)
+        {
+          cant_razones++;
+
+          Agregar('razon', respuesta);
+
+          VerificarCant();
+        }
+      }
+    });
+
+    return false;
+  });
+
+  jQuery('#remover_razon').click(function()
+  {
+    cant_razones--;
+
+    jQuery('#razon_agregada:last-child').remove();
+
+    VerificarCant();
+
+    return false;
+  });
+
+  jQuery('#agregar_acciones').click(function()
+  {
+    Agregar('acciones');
+
+    jQuery('#agregar_acciones').hide();
+
+    jQuery('#remover_acciones').show();
+
+    return false;
+  });
+
+  jQuery('#remover_acciones').click(function()
+  {
+    jQuery('#accion_agregada:last-child').remove();
+
+    jQuery('#agregar_acciones').show();
+
+    jQuery('#remover_acciones').hide();
+
+    return false;
+  });
+
+  jQuery('#agregar_compromiso').click(function()
+  {
+    var cant = prompt('Ingrese la cantidad de responsables para el compromiso:', 1);
     
-    VerificarCantFotos();
+    if (!isNaN(cant) && cant!=null && cant>0)
+    {
+      cant_compromisos++;
+
+      Agregar('compromiso',cant);
+
+      VerificarCant();
+    }
+    else if (isNaN(cant))
+      alert ('Error:: No introdujo un valor númerico');
+    else if (cant == 0)
+      alert ('Error:: Debe introducir al menos un responsable');
+
+    return false;
+  });
+  
+  jQuery('#remover_compromiso').click(function()
+  {
+    cant_compromisos--;
+
+    jQuery('#compromiso_agregado:last-child').remove();
+
+    VerificarCant();
 
     return false;
   });
   
 });
 
-function VerificarCantFotos() {
-  if (cant_fotos == 0)
-    jQuery('#remover_imagen').hide();
-  else
-  {
-    jQuery('#remover_imagen').show();
-  }
-}

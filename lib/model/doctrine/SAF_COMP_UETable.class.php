070f81f2358c1c96f9359a2b04790355c9cbd4bb
Rosman Torres - Trabajo <>
2013-07-19 15:12:30 -0400

    - Agregando método getCompromisosDetalladosDeLaUnidad (Falta terminarlo)

--- lib/model/doctrine/SAF_COMP_UETable.class.php
+++ lib/model/doctrine/SAF_COMP_UETable.class.php
@@ -16,8 +16,8 @@ class SAF_COMP_UETable extends Doctrine_Table
   public static function getInstance()
   {
     return Doctrine_Core::getTable('SAF_COMP_UE');
-  }  
-   
+  }
+
   /**
    * Método que obtiene con PDO todos los compromisos pendientes o terminados 
    * para una determinada unidad.
@@ -29,7 +29,7 @@ class SAF_COMP_UETable extends Doctrine_Table
   public function getIndicadorDeCompromisos($id_unidad, $status)
   {
     $conexion = Doctrine_Manager::getInstance()->getConnection("schema_saf");
-
+    
     $consulta = "
       SELECT comp_ue.status AS STATUS, COUNT (comp_ue.status) AS CANT_COMPROMISO
       FROM SAF_COMP_UE comp_ue    
@@ -40,7 +40,7 @@ class SAF_COMP_UETable extends Doctrine_Table
 
     return $sentencia->fetchAll(PDO::FETCH_BOTH);
   }
-  
+
   /**
    * Método que toma todas las unidades retornandolas con coma (,)
    * para las categorias del gráfico (indicador) que se genera con HighCharts.
@@ -67,12 +67,12 @@ class SAF_COMP_UETable extends Doctrine_Table
    * @return implode
    */
   public function getSeriesDelIndicadorDeCompromisos($unidades, $status)
-  {    
+  {
     $data_series = array();
 
     foreach ($unidades as $unidad)
     {
-      $resultset = $this->getIndicadorDeCompromisos($unidad->getId(),$status);
+      $resultset = $this->getIndicadorDeCompromisos($unidad->getId(), $status);
 
       if ($resultset)
       {
@@ -86,4 +86,39 @@ class SAF_COMP_UETable extends Doctrine_Table
 
     return implode(',', $data_series);
   }
+
+  public function getCompromisosDetalladosDeLaUnidad($id_unidad)
+  {
+    $conexion = Doctrine_Manager::getInstance()->getConnection("schema_saf");
+    
+    $consulta = "
+SELECT 
+ue.nombre AS NOMBRE_UE,
+comp.descripcion AS DESC_COMP,
+comp.created_at AS F_COMP,
+comp.f_duracion_estimada AS F_DURACION_EST,
+comp_ue.id AS ID_COMP,
+comp_ue.status AS STATUS_COMP,
+comp_ue.acciones AS ACCIONES,
+minuta.cod_min AS COD_MIN,
+minuta.created_at AS F_MINUTA,
+evento.c_evento_d AS RI,
+evento.circuito AS CIRCUITO,
+evento.f_hora_ini AS FECHA_CASO
+FROM SAF_COMP_UE comp_ue
+INNER JOIN SAF_UNIDAD_EQUIPO ue ON ue.id = comp_ue.id_ue
+INNER JOIN SAF_VARIO comp ON comp.id = comp_ue.id_compromiso
+INNER JOIN SAF_EVENTO evento ON evento.id = comp.id_evento
+INNER JOIN SAF_CONVOCATORIA_CAF conv_caf ON conv_caf.id = evento.id_convocatoria
+INNER JOIN SAF_MINUTA minuta ON minuta.id_convocatoria = conv_caf.id
+WHERE comp.tipo = 'COMPROMISO' AND comp_ue.id_ue = $id_unidad AND minuta.id_convocatoria = evento.id_convocatoria
+      ";
+    
+    $consulta = $consulta . " AND comp_ue.status = 'TERMINADO'";
+
+    $sentencia = $conexion->execute($consulta);
+
+    return $sentencia->fetchAll(PDO::FETCH_BOTH);
+  }
+
 }
